5bbff9ec4e3ce48eba661ed580d92766ac153b72
asheeshcric <ashiz2013@gmail.com>
2018-01-11 20:01:14 +0545

    Made some minor changes

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 from TogglAPI import TogglAPI
 
-tog = TogglAPI(api_token="068557f61a05f6973617f6b4ff653adb")                #Enter your API_Token here.
+tog = TogglAPI(api_token="")                #Enter your API_Token here.
 
 updates = tog.display_todays_updates()      #Returns a list of Today's working descriptions
 
